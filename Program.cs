6e86853bc398c6b33648cd78429e3fdc6ec13f54
ArtemLevin <python_study_2022@mail.ru>
2023-01-24 02:00:16 +0300

    Добавлен метод для печати массива и для создания массива с заданной длиной строки

--- Program.cs
+++ Program.cs
@@ -1,14 +1,44 @@
 ﻿// method to create an array filled with string elements
 
+Random rnd = new Random();
+int lengthOfArray = rnd.Next(5, 11); // рандомизируем длину массива
 
-void CreateAndPrintStringArray()
+void PrintArray(string[] array) // метод для  вывода массива с учетом расстановки скобок
 {
-    Random rnd = new Random();
-    int lengthOfArray = rnd.Next(0, 11); // рандомизируем длину массива
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0 & array.Length > 1)
+        {
+            Console.Write("[" + " " + array[i] + " ");
+        }
+
+        if (i == 0 & array.Length <2)
+        {
+            Console.Write("[" + " " + array[i] + " " + "]");
+        }
+
+        if (i != 0 & i != array.Length-1 )
+        {
+            Console.Write(array[i] + " ");
+        }
+
+        if (i == array.Length-1 & array.Length > 1 )
+        {
+            Console.Write(array[i] + " " + "]");
+        }
+        if (array.Length == 0)
+        {
+            Console.WriteLine("[ ]");
+        }
+    }
+}
+
+string[] CreateAndPrintStringArray(int lengthOfArray)
+{
     string[] StringArray = new string[lengthOfArray]; // инициализируем новый массив
     for (int i = 0; i < lengthOfArray; i++)
     {
-        int StringLength = rnd.Next(0, 11); // рандомизируем длину нового слова
+        int StringLength = rnd.Next(1, 11); // рандомизируем длину нового слова
         string str = ""; // инициализируем новое слово
         char letter; // инициализируем новую букву для нового слова
         for (int j = 0; j < StringLength; j++)
@@ -17,14 +47,42 @@ void CreateAndPrintStringArray()
             letter = Convert.ToChar(randValue + 65); // переводим ее в тип char
             str +=letter ; // добавляем новую букву к формируемому слову
         }
-        StringArray[i] = str;
-        
+        StringArray[i] = str; // добавляем новое слово в массив
     }
-    for (int i = 0; i < lengthOfArray; i++)
-    {
-        Console.Write(StringArray[i] + " ");
-    }
-
+    PrintArray(StringArray);
+    return StringArray;
 }
 
-CreateAndPrintStringArray();
+void NotMoreThanThreeSymbolsStringArray () // метод для определения слов заданной длины и записи их в новый массив
+{
+    string[] stringArray = new string [lengthOfArray];
+    stringArray = CreateAndPrintStringArray(lengthOfArray); // создаем массив из слов при помощи метода
+
+    int newStringArrayLength = 0;
+
+    for (int i = 0; i < lengthOfArray; i++) // цикл для определения длины массива с элементами длиной не больше 3 символов
+    {
+        int length = stringArray[i].Length;
+        if (length <= 3)
+        {
+            newStringArrayLength++;      
+        }
+    }
+
+    Console.WriteLine();
+    string[] newStringArray = new string[newStringArrayLength]; // инициализируем новый массив
+    int count = 0; // счетчик для количества индексации слов в номо массиве
+    for (int i = 0; i < stringArray.Length; i++) // цикл для определения слов заданной длины и записи их в новый массив
+    {
+        int length = stringArray[i].Length;
+        if (length <= 3)
+        {
+            newStringArray[count] = stringArray[i];
+            Console.Write (newStringArray[count] + " ");
+            count++;
+        }
+    }
+    PrintArray(newStringArray);
+}
+
+NotMoreThanThreeSymbolsStringArray ();
